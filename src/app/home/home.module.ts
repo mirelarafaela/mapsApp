4b5eece4f9f03146de044dd813682ed9f8ca5a89
Mirela Rafaela Machado <mirela.rafaela@hotmail.com>
2020-11-30 20:24:16 -0300

    Google Maps

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   imports: [
@@ -14,6 +14,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [Geolocation]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,54 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map: google.maps.Map;
+  minhaPosicao: google.maps.LatLng;
 
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
+  constructor(private geolocation: Geolocation) {}
+
+ionViewWillEnter(){
+  this.exibirMapa();
+}
+
+  exibirMapa(){
+    const posicao = new google.maps.LatLng(-22.194082, -48.778885);
+    const opcoes = {
+      center: posicao,
+      zoom: 1,
+      disableDefaultUI: true
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
+
+    this.buscarPosicao();
+  }
+
+
+  buscarPosicao(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+
+      this.minhaPosicao = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+      this.irParaMinhaPosicao();
+
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
+
+  irParaMinhaPosicao(){
+    this.map.setCenter(this.minhaPosicao);
+    this.map.setZoom(15);
+
+    const marker = new google.maps.Marker({
+      position: this.minhaPosicao,
+      title: 'Minha Posição',
+      animation: google.maps.Animation.BOUNCE,
+      map: this.map
+    });
+  }
 }
